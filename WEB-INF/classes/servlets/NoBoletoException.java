8e8302961e6c633e8351eae9eab18b52e2e12680
Jayme <contato@doois.com.br>
2012-05-22 15:02:48 -0300

    package

---
+++ WEB-INF/classes/servlets/NoBoletoException.java
@@ -0,0 +1,23 @@
+package servlets;
+
+public class NoBoletoException extends Exception
+{
+	public NoBoletoException()
+	{
+		// TODO Auto-generated constructor stub
+		super();
+	}
+
+	public NoBoletoException(String string)
+	{
+		// TODO Auto-generated constructor stub
+		super(string);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+}
